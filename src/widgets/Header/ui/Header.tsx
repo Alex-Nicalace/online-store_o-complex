5d5353dd21e0e71492a6d915efbb4e4a83e9c328
Alex-Nicalace <anicalace@gmail.com>
2025-06-04 18:04:55 +0300

    Header: create

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,12 +1,13 @@
-import { Container } from '@shared/ui/Container';
 import cn from 'classnames';
-import cls from './Header.module.scss';
 
-type HeaderProps = { className?: string };
+import Container from '@shared/ui/Container';
+import cls from './Header.module.scss';
+import type { HeaderProps } from './Header.types';
+
 function Header({ className }: HeaderProps) {
   return (
-    <Container tag="header">
-      <h1 className={cn(cls.Header, className)}>тестовое задание</h1>
+    <Container tag="header" className={cn(cls.Header, className)}>
+      <h1 className={cls.title}>тестовое задание</h1>
     </Container>
   );
 }
