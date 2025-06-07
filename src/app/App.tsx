1cdf18cd6b88aa3b6cd4b301ca0086c597d425b2
Alex-Nicalace <anicalace@gmail.com>
2025-06-07 16:27:03 +0300

    App: use Providers

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
 import { MainPage } from '@pages/MainPage';
 import './styles/index.scss';
 import { Header } from '@widgets/Header';
+import { Providers } from './providers';
 function App() {
   return (
-    <>
+    <Providers>
       <Header />
       <MainPage />
-    </>
+    </Providers>
   );
 }
 
