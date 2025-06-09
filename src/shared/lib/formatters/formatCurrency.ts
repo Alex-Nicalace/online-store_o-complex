55935664d8d477ce2618a1b62158ba8554d4916f
Alex-Nicalace <anicalace@gmail.com>
2025-06-09 13:51:04 +0300

    formatCurrency: create

---
+++ src/shared/lib/formatters/formatCurrency.ts
@@ -0,0 +1,8 @@
+export function formatCurrency(value: number) {
+  return new Intl.NumberFormat('ru-RU', {
+    style: 'currency',
+    currency: 'RUB',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  }).format(value);
+}
--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,2 +1,3 @@
 export * from './html';
 export * from './store';
+export * from './formatters';
