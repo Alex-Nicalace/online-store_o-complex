72a6be3f5cda3786c49fbda356d646ff3edbe105
Alex-Nicalace <anicalace@gmail.com>
2025-06-16 09:20:43 +0300

    Loader: add prop small

--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -3,7 +3,12 @@ import cls from './Loader.module.scss';
 
 type LoaderProps = {
   className?: string;
+  isSmall?: boolean;
 };
-export function Loader({ className }: LoaderProps) {
-  return <div className={cn(cls.Loader, className)}></div>;
+export function Loader({ className, isSmall }: LoaderProps) {
+  return (
+    <span
+      className={cn(cls.Loader, className, { [cls.Loader_small]: isSmall })}
+    ></span>
+  );
 }
