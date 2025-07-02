ccd0fe45289e4378d904d1486ced29f212653ca2
Alex-Nicalace <anicalace@gmail.com>
2025-07-02 16:40:17 +0300

    MakeOrder: refactoring

--- src/features/make-order/ui/MakeOrder.tsx
+++ src/features/make-order/ui/MakeOrder.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useActionState, useEffect } from 'react';
 import cn from 'classnames';
 
 import { clear, selectCartList } from '@entities/Cart';
-import { createOrder } from '@shared/api';
+import { createOrder, type OrderCartEntity } from '@shared/api';
 import {
   registerMask,
   useAppDispatch,
@@ -14,57 +14,71 @@ import { Button, Input, useNotification } from '@shared/ui';
 import { useMapCartItemsToOrderCartEntitys } from '../lib';
 import cls from './MakeOrder.module.scss';
 
+interface SubmitStatus {
+  success: boolean;
+  message: string;
+}
+interface SubmitPayload {
+  phone: string;
+  cart: OrderCartEntity[];
+}
+
+// Функция обработки отправки данных
+async function handleDataSend(_: SubmitStatus | null, payload: SubmitPayload) {
+  try {
+    const response = await createOrder(payload);
+    if (response.success) {
+      return { success: true, message: 'Заказ успешно создан' };
+    } else return { success: false, message: response.error };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { success: false, message: error.message };
+    } else {
+      return { success: false, message: 'Unknown error' };
+    }
+  }
+}
+
 type MakeOrderProps = {
   className?: string;
 };
-
 export default function MakeOrder({ className }: MakeOrderProps) {
   const [phone, setPhone] = useLocalStorageState('', 'order-phone');
   const cartList = useAppSelector(selectCartList);
   const cart = useMapCartItemsToOrderCartEntitys(cartList);
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const { showNotification } = useNotification();
   const dispatch = useAppDispatch();
+  const [submitStatus, submitAction, isSubmitting] = useActionState<
+    SubmitStatus | null,
+    SubmitPayload
+  >(handleDataSend, null);
 
   const mask = '+7 (___) ___-__-__';
   const regexp = /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/;
   const options = { isHideMask: false, setValue: setPhone };
   const handlers = registerMask(mask, options);
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const dataToSend = { phone: phone.replace(/\D/g, ''), cart };
-    setIsSubmitting(true);
-    try {
-      const response = await createOrder(dataToSend);
-      if (response.success) {
-        showNotification({
-          type: 'success',
-          message: 'Заказ успешно создан',
-        });
-        dispatch(clear());
-      } else {
-        showNotification({
-          type: 'error',
-          message: response.error,
-        });
-      }
-    } catch (error) {
+  useEffect(() => {
+    if (submitStatus) {
       showNotification({
-        type: 'error',
-        message:
-          error instanceof Error
-            ? error.message
-            : 'Неизвестная ошибка при создании заказа',
+        type: submitStatus.success ? 'success' : 'error',
+        message: submitStatus.message,
       });
+      if (submitStatus.success) {
+        dispatch(clear());
+      }
     }
-    setIsSubmitting(false);
+  }, [submitStatus, dispatch, showNotification]);
+
+  function handleAction() {
+    const dataToSend = { phone: phone.replace(/\D/g, ''), cart };
+    submitAction(dataToSend);
   }
 
   return (
     <form
       className={cn(cls.MakeOrder, className)}
-      onSubmit={handleSubmit}
+      action={handleAction}
       name="createOrder"
     >
       <label htmlFor="phone" className="sr-only">
